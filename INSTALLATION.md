14baeb1f6bba8ac2d2748840f17b0f50f01f78ba
Dominik <dominik@herkel.at>
2018-07-08 21:48:34 +0200

    implemented docker-compose container setup; added prometheus whitebox monitoring for ovs_gnxi interface;

--- INSTALLATION.md
+++ INSTALLATION.md
@@ -15,14 +15,56 @@ go build -o ovs_gnxi
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o cod
 ```
 
+## Push Container to Registry
+
+```bash
+docker login
+docker pull coolbreeze/ovs-gnxi:ovs
+docker pull coolbreeze/ovs-gnxi:gnxi
+docker build -t coolbreeze/ovs-gnxi:ovs -f docker/ovs/Dockerfile .
+docker build -t coolbreeze/ovs-gnxi:gnxi -f docker/gnxi/Dockerfile .
+docker push coolbreeze/ovs-gnxi:ovs
+docker push coolbreeze/ovs-gnxi:gnxi
+```
+
+## Docker-Compose
+
+```bash
+docker login
+docker-compose pull
+docker-compose build
+docker-compose push
+```
+
 ## Run
 
 **Run Container:**
 ```bash
-docker run -d -p 32123:32123 --name ovs-gnxi hub.docker.com/coolbreeze/ovs-gnxi/ovs-gnxi:latest
+docker-compose up -d
+docker-compose down
 ```
 
 **Remove all stopped containers:**
 ```bash
 docker ps -a -q | % { docker rm $_ }
+```
+
+**Remove all images:**
+```bash
+docker images -a -q | % { docker rmi $_ -f }
+```
+
+**Connect to Container:**
+```bash
+docker ps -aqf "name=gnxi"
+docker exec -i -t $CONTAINER_ID bash
+```
+
+## Go
+
+### Govendor
+
+```bash
+govendor init
+govendor add +external
 ```
--- ovs_gnxi.go
+++ ovs_gnxi.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/op/go-logging"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"fmt"
+)
+
+var (
+	logModule = "ovs-gnxi"
+	log       = logging.MustGetLogger(logModule)
+)
 
 func main() {
-	fmt.Printf("Starting Open vSwitch gNXI interface\n")
+	defer os.Exit(0)
+	defer log.Info("Exiting Open vSwitch gNXI interface\n")
+
+	log.Info("Starting Open vSwitch gNXI interface\n")
+
+	prometheusInstance, err := NewPrometheusMonitoringInstance("0.0.0.0", "8080")
+	if err != nil {
+		log.Errorf("Unable to configure Prometheus Monitoring: %v", err)
+		os.Exit(1)
+	}
+	go prometheusInstance.StartPrometheus()
+
+	run()
+}
+
+func run() {
+	select {}
+}
+
+type PrometheusMonitoringInstance struct {
+	IPAddress			string
+	Port				string
+	ErrorsGaugeMetric	prometheus.Gauge
+}
+
+func NewPrometheusMonitoringInstance(ipAddress, port string) (*PrometheusMonitoringInstance, error) {
+	p := PrometheusMonitoringInstance{IPAddress: ipAddress, Port: port}
+	p.InitializeMetrics()
+	p.RegisterMetrics()
+	return &p, nil
+}
+
+func (p *PrometheusMonitoringInstance) InitializeMetrics() {
+	p.ErrorsGaugeMetric = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "cod_errors",
+		Help: "The number of errors CoD experienced during runtime.",
+	})
+	p.ErrorsGaugeMetric.Set(0)
+}
+
+func (p *PrometheusMonitoringInstance) RegisterMetrics() {
+	prometheus.MustRegister(p.ErrorsGaugeMetric)
+}
+
+func (p *PrometheusMonitoringInstance) StartPrometheus() {
+	log.Infof("Starting prometheus on %v:%v...", p.IPAddress, p.Port)
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v:%v", p.IPAddress, p.Port), nil))
 }
